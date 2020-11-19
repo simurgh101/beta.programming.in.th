6aa656aaf7fbeb24d8289b541a17fe9500dcb8be
Simurgh101(Scotth) <suthiwatpraesi@hotmail.com>
2020-11-19 17:52:08 +1100

    stupid stuff

--- Templates/A.cpp
+++ Templates/A.cpp
@@ -29,37 +29,17 @@
 using namespace std;
 
 #define _USE_MATH_DEFINES
-#define abs(a) (((a) > 0) ? (a) : (-(a)))
 #define M_PI 3.14159265358979323846 /* pi */
 #define LL long long int
 #define vt vector
 
-void solve()
-{
-  char s[101];
-
-  cin >> s;
-  int l = strlen(s);
-  sort(s, s + l);
+int main() {
+  string s;
   int count = 0;
-  for (int i = 0; i < l; i++)
-  {
-    if (s[i] != s[i + 1])
-    {
-      count++;
-    }
-  }
-  if (count % 2 == 0)
-  {
-    cout << "CHAT WITH HER!\n";
-  }
-  else
-  {
-    cout << "IGNORE HIM!\n";
-  }
-}
 
-int main()
-{
-  solve();
+  for (char c:s) {
+    if (isupper(c)) c++;
+  }
+  if (count == size(s))
+  return 0;
 }
