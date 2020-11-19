6bffe6f1827a5d1b8e721b9b78d97d30e6b80253
Simurgh101(Scotth) <suthiwatpraesi@hotmail.com>
2020-11-19 17:50:54 +1100

    0005

---
+++ easy/0005.cpp
@@ -0,0 +1,42 @@
+#include <utility>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <array>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+#define _USE_MATH_DEFINES
+#define M_PI 3.14159265358979323846 /* pi */
+#define LL long long int
+#define vt vector
+
+int main() {
+     double a,b;
+     scanf("%lf %lf" ,&a ,&b);
+     printf("%lf" , sqrt(a*a + b*b));
+     
+     return 0;
+}
